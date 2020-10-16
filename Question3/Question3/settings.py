2c8e6391cece7c597fa02316611764be1ef07b2a
marc <marc-antoine.plourde1@uqac.ca>
2020-10-16 16:29:07 -0400

    nothing

--- Question3/Question3/settings.py
+++ Question3/Question3/settings.py
@@ -125,6 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=(str(BASE_DIR.joinpath('static')),)
+STATICFILES_DIRS=(BASE_DIR.joinpath('static'),)
 
 
